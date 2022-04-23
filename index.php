1ec265626d0531937a1b11f8aba4e008bdbfe2c6
Darling <69660798+d4rl@users.noreply.github.com>
2022-04-23 01:59:18 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="./css/main.css">
+	<title>D4rl's portofolio website</title>
+</head>
+<body>
+	<h1 class="center">Felipe's Portofolio</h1>
+	<h3 class="center"><a href="./pages/about_me.html">About-Me.txt</a>   <a href="./pages/social_medias.html">Social-Medias.txt</a>   <a href="./pages/contact.html">Contant.txt</a>   </h3>
+</body>
+</html>
